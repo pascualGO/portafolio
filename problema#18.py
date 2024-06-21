9090cc3103fe1202df84eaa93c371b11b24e11e7
pascual gonzalez <pascualg152@gmail.com>
2024-06-21 09:21:36 -0500

    Add files via upload

---
+++ problema#18.py
@@ -0,0 +1,13 @@
+print("Número negativo, positivo o cero")
+print("--------------------------------")
+
+numero=int(input("Ingrese un número: "))
+   
+if numero<0:
+       print("El número es negativo")
+elif numero>0:
+       print("El número es positivo")
+else:
+       print("El número es cero")
+       
+   
